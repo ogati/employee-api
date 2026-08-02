9203e851e61cb6d0c49fbae727d832bf77f88ffb
Lei Liu <Lei.Java@gmail.com>
2026-08-02 11:37:47 -0400

    Refactored

--- src/main/java/com/example/employee/api/employee/EmployeeController.java
+++ src/main/java/com/example/employee/api/employee/EmployeeController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
